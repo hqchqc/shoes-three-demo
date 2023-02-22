94f204698da2c0976a6c0878a1c3765b53ec8a8f
qingchanghuang <qingchanghuang@deeproute.ai>
2023-02-22 17:21:01 +0800

    feat: 增加立体感

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,11 +12,10 @@
     </hgroup>
 
     <section class="pl-10 pr-10">
-      <div class="bg-white">
+      <!-- <div class="bg-white">
         <canvas id="three" class="w-full mt-10"></canvas>
-
-        <span>21321</span>
-      </div>
+      </div> -->
+      <canvas id="three" class="w-full mt-10"></canvas>
     </section>
   </div>
 </template>
@@ -50,9 +49,10 @@ const getCurrentTime = () => {
 const initThree = () => {
   console.log("1", 1);
   const scene = new Scene();
-  scene.background = new Color("#dfd8d5");
+  // scene.background = new Color("0x000000");
   const canvas = document.querySelector("#three") as HTMLCanvasElement;
   const renderer = new WebGLRenderer({ canvas, antialias: true });
+  renderer.setClearColor(0xeeeeee, 0.0);
   const camera = new PerspectiveCamera(
     105,
     window.innerWidth / window.innerHeight,
@@ -63,95 +63,95 @@ const initThree = () => {
   camera.position.z = 5;
 
   const loader = new GLTFLoader().setPath("/shoes/");
-  // loader.load("scene.gltf", (gltf) => {
-  //   gltf.scene.position.z = 0.01;
+  loader.load("scene.gltf", (gltf) => {
+    gltf.scene.position.z = 0.01;
 
-  //   gltf.scene.traverse((child) => {
-  //     const explosionTexture = new TextureLoader().load(
-  //       "/shoes/textures/material_0_diffuse.jpeg"
-  //     );
+    gltf.scene.traverse((child) => {
+      const explosionTexture = new TextureLoader().load(
+        "/shoes/textures/material_0_diffuse.jpeg"
+      );
 
-  //     explosionTexture.flipY = false;
+      explosionTexture.flipY = false;
 
-  //     const material = new MeshBasicMaterial({
-  //       map: explosionTexture,
-  //     });
+      const material = new MeshBasicMaterial({
+        map: explosionTexture,
+      });
 
-  //     (child as Mesh).material = material;
-  //   });
+      (child as Mesh).material = material;
+    });
 
-  //   gltf.scene.rotateX(-Math.PI / 2);
-  //   gltf.scene.rotateY(Math.PI / 2);
-  //   gltf.scene.translateY(-3);
-  //   gltf.scene.translateZ(-1);
-  //   // gltf.scene.rotateZ(Math.PI * 2);
-  //   // gltf.scene.rotateX(Math.PI / 4);
+    gltf.scene.rotateX(-Math.PI / 2);
+    gltf.scene.rotateY(Math.PI / 2);
+    gltf.scene.translateY(-3);
+    gltf.scene.translateZ(-1);
+    // gltf.scene.rotateZ(Math.PI * 2);
+    // gltf.scene.rotateX(Math.PI / 4);
 
-  //   scene.add(gltf.scene);
-  // });
+    scene.add(gltf.scene);
+  });
 
-  // loader.load("scene.gltf", (gltf) => {
-  //   gltf.scene.position.z = 0.01;
+  loader.load("scene.gltf", (gltf) => {
+    gltf.scene.position.z = 0.01;
 
-  //   gltf.scene.traverse((child) => {
-  //     const explosionTexture = new TextureLoader().load(
-  //       "/shoes/textures/material_0_diffuse.jpeg"
-  //     );
+    gltf.scene.traverse((child) => {
+      const explosionTexture = new TextureLoader().load(
+        "/shoes/textures/material_0_diffuse.jpeg"
+      );
 
-  //     explosionTexture.flipY = false;
+      explosionTexture.flipY = false;
 
-  //     const material = new MeshBasicMaterial({
-  //       map: explosionTexture,
-  //     });
+      const material = new MeshBasicMaterial({
+        map: explosionTexture,
+      });
 
-  //     (child as Mesh).material = material;
-  //   });
+      (child as Mesh).material = material;
+    });
 
-  //   gltf.scene.translateX(1);
-  //   gltf.scene.translateZ(1);
-  //   gltf.scene.rotateX(Math.PI / 2);
-  //   gltf.scene.rotateY(-Math.PI / 2);
+    gltf.scene.translateX(1);
+    gltf.scene.translateZ(1);
+    gltf.scene.rotateX(Math.PI / 2);
+    gltf.scene.rotateY(-Math.PI / 2);
 
-  //   scene.add(gltf.scene);
-  // });
+    scene.add(gltf.scene);
+  });
 
-  // const boxLoader = new GLTFLoader().setPath("/box/");
-  // boxLoader.load("scene.gltf", (gltf) => {
-  //   gltf.scene.traverse((child) => {
-  //     const explosionTexture = new TextureLoader().load(
-  //       "/box/textures/BOTTOM_baseColor.png"
-  //     );
+  const boxLoader = new GLTFLoader().setPath("/box/");
+  boxLoader.load("scene.gltf", (gltf) => {
+    gltf.scene.traverse((child) => {
+      const explosionTexture = new TextureLoader().load(
+        "/box/textures/BOTTOM_baseColor.png"
+      );
 
-  //     if (child.name === "Plane_Plane_002_Material_001_TOP_0") {
-  //       const lidTexture = new TextureLoader().load(
-  //         "/box/textures/material_baseColor.png"
-  //       );
-  //       lidTexture.flipY = false;
-  //       const material = new MeshBasicMaterial({
-  //         map: lidTexture,
-  //       });
-  //       (child as Mesh).material = material;
+      if (child.name === "Plane_Plane_002_Material_001_TOP_0") {
+        const lidTexture = new TextureLoader().load(
+          "/box/textures/material_baseColor.png"
+        );
+        lidTexture.flipY = false;
+        const material = new MeshBasicMaterial({
+          map: lidTexture,
+        });
+        (child as Mesh).material = material;
 
-  //       child.rotateX(-(Math.PI * 3) / 4);
+        child.rotateX(-(Math.PI * 3) / 4);
 
-  //       return;
-  //     }
+        return;
+      }
 
-  //     console.log("child", child);
-  //     explosionTexture.flipY = false;
+      console.log("child", child);
+      explosionTexture.flipY = false;
 
-  //     const material = new MeshBasicMaterial({
-  //       map: explosionTexture,
-  //     });
+      const material = new MeshBasicMaterial({
+        map: explosionTexture,
+      });
 
-  //     (child as Mesh).material = material;
-  //   });
+      (child as Mesh).material = material;
+    });
 
-  //   gltf.scene.translateY(-2);
-  //   gltf.scene.translateZ(2);
-  //   gltf.scene.scale.set(22, 22, 22);
-  //   scene.add(gltf.scene);
-  // });
+    gltf.scene.translateY(-2);
+    gltf.scene.translateZ(2);
+    gltf.scene.scale.set(22, 22, 22);
+    scene.add(gltf.scene);
+  });
 
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.enableDamping = true;
